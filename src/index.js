3e62efd83adb7749457826c44ec61aa1fc584781
bob <jiafeifei12345@163.com>
2019-11-21 17:28:53 +0800

    优化
    1. 页面完善用户输入
    2. 增加接壤色设定

--- src/index.js
+++ src/index.js
@@ -14,15 +14,30 @@ ctx.fillStyle = "#000000";
 ctx.fillRect(10, 10, COLOR_WIDTH, COLOR_HEIGHT);
 ctx.fillStyle = "#FF0000";
 ctx.fillRect(10, 10, OTHER_COLOR_WIDTH, OTHER_COLOR_HEIGHT);
+ctx.fillRect(20, 20, OTHER_COLOR_WIDTH, OTHER_COLOR_HEIGHT);
+ctx.fillStyle = "#FFFF00";
+ctx.fillRect(30, 30, OTHER_COLOR_WIDTH, OTHER_COLOR_HEIGHT);
+ctx.fillRect(40, 40, OTHER_COLOR_WIDTH, OTHER_COLOR_HEIGHT);
+ctx.fillStyle = "#0000FF";
+ctx.fillRect(10, 50, 40, 40);
 const imgD = ctx.getImageData(10, 10, IMAGE_WIDTH, IMAGE_HEIGHT);
 
-function getRGBAI(imageData, i) {
+function rc2i(imageData, r, c) {
+  //input pixel row col ,get pixel index
+  const { width } = imageData;
+  return r * width + c;
+}
+function getRGBAIByIndex(imageData, i) {
   const { data } = imageData;
   const r = data[4 * i + 0];
   const g = data[4 * i + 1];
   const b = data[4 * i + 2];
   const a = data[4 * i + 3];
   return { r, g, b, a, i };
+}
+function getRGBAIByRowCol(imageData, r, c) {
+  const i = rc2i(imageData, r, c);
+  return getRGBAIByIndex(imageData, i);
 }
 function pixelArray(imageData) {
   //pixelArray = [{r,g, b, a, i}]
@@ -31,7 +46,7 @@ function pixelArray(imageData) {
   result = data.reduce((p, c, i, a) => {
     const pixelNum = Math.floor(i / 4);
     const pixelIdx = i % 4;
-    let rst = pixelIdx === 0 ? [...p, getRGBAI(imageData, pixelNum)] : p;
+    let rst = pixelIdx === 0 ? [...p, getRGBAIByIndex(imageData, pixelNum)] : p;
     return rst;
   }, []);
   return result;
@@ -68,7 +83,8 @@ function isPixelNull(pixelObj) {
   const { r, g, b, a, i } = pixelObj;
   return r === 0 && g === 0 && b === 0 && a === 0;
 }
-function isPixelAtBorder(imageData, i) {
+function isPixelAtBorder(imageData, pixelObj) {
+  const { i } = pixelObj;
   const rows = imageData.height;
   const cols = imageData.width;
   const rowIdx = Math.floor(i / cols);
@@ -77,13 +93,69 @@ function isPixelAtBorder(imageData, i) {
     rowIdx === 0 || rowIdx === rows - 1 || colIdx === 0 || colIdx === cols - 1
   );
 }
-function nearOtherColor(imageData, i) {
+function nearOtherColor(imageData, pixelObj, neighborHexColor) {
+  const { r, g, b, a, i } = pixelObj;
   let result = false;
   const rows = imageData.height;
   const cols = imageData.width;
   const rowIdx = Math.floor(i / cols);
   const colIdx = i % cols;
   const LT = {};
+  let neighbors = [];
+  for (let i = rowIdx - 1; i < rowIdx + 2; i++) {
+    for (let j = colIdx - 1; j < colIdx + 2; j++) {
+      if (
+        i >= 0 &&
+        i <= rows - 1 &&
+        j >= 0 &&
+        j <= cols - 1 &&
+        (i !== rowIdx || j !== colIdx)
+      ) {
+        neighbors.push([i, j]);
+      }
+    }
+  }
+
+  const someNeighborIsDifferentColor = neighbors.some(v => {
+    const [ri, ci] = v;
+    const { r: rNeighbor, g: gNeighbor, b: bNeighbor } = getRGBAIByRowCol(
+      imageData,
+      ri,
+      ci
+    );
+    return (
+      rgb2Hex([rNeighbor, gNeighbor, bNeighbor].join(",")) !==
+      rgb2Hex([r, g, b].join(","))
+    );
+  });
+  const allDifferentColorNeighbors = neighbors.filter(v => {
+    const [ri, ci] = v;
+    const { r: rNeighbor, g: gNeighbor, b: bNeighbor } = getRGBAIByRowCol(
+      imageData,
+      ri,
+      ci
+    );
+    return (
+      rgb2Hex([rNeighbor, gNeighbor, bNeighbor].join(",")) !==
+      rgb2Hex([r, g, b].join(","))
+    );
+  });
+  const allDifferentColorNeighborsHasSameColor =
+    !!!neighborHexColor ||
+    !allDifferentColorNeighbors.some(v => {
+      const [ri, ci] = v;
+      const { r: rNeighbor, g: gNeighbor, b: bNeighbor } = getRGBAIByRowCol(
+        imageData,
+        ri,
+        ci
+      );
+      return (
+        rgb2Hex([rNeighbor, gNeighbor, bNeighbor].join(",")) !==
+        neighborHexColor
+      );
+    });
+  result =
+    someNeighborIsDifferentColor && allDifferentColorNeighborsHasSameColor;
   return result;
 }
 function isSrcColor(pixelObj, srcHexColor) {
@@ -93,31 +165,45 @@ function isSrcColor(pixelObj, srcHexColor) {
 
   return result;
 }
-function borderPureColor(imageData, srcHexColor, destHexColor) {
+function borderPureColor(
+  imageData,
+  srcHexColor,
+  borderHexColor,
+  neighborHexColor
+) {
   const rows = imageData.height;
   const cols = imageData.width;
   const pixelArr = pixelArray(imageData);
   const newImageData = pixelArr.reduce((p, c) => {
     const { i } = c;
+    // 如果需要勾 imagedata 的边界，解注释此处，把下面的接成 else if 即可
+    // if (
+    //   !isPixelNull(c) &&
+    //   isSrcColor(c, srcHexColor) &&
+    //   isPixelAtBorder(imageData, c)
+    // ) {
+    //   // not null && at image data  border
+    //   const [destR, destG, destB] = hex2Rgb(borderHexColor);
+    //   // let newPixelArr = [destR, destG, destB, 255];
+    //   // console.log(`newPixelArr:`, newPixelArr);
+    //   p.data[4 * i + 0] = destR;
+    //   p.data[4 * i + 1] = destG;
+    //   p.data[4 * i + 2] = destB;
+    //   p.data[4 * i + 3] = 255;
+    // }
     if (
       !isPixelNull(c) &&
       isSrcColor(c, srcHexColor) &&
-      isPixelAtBorder(imageData, i)
+      nearOtherColor(imageData, c, neighborHexColor)
     ) {
-      // not null && at image data  border
-      const [destR, destG, destB] = hex2Rgb(destHexColor);
+      // not null && not at image data border , but near othercolor
+      const [destR, destG, destB] = hex2Rgb(borderHexColor);
       // let newPixelArr = [destR, destG, destB, 255];
       // console.log(`newPixelArr:`, newPixelArr);
       p.data[4 * i + 0] = destR;
       p.data[4 * i + 1] = destG;
       p.data[4 * i + 2] = destB;
       p.data[4 * i + 3] = 255;
-    } else if (
-      !isPixelNull(c) &&
-      isSrcColor(c, srcHexColor) &&
-      nearOtherColor(imageData, i)
-    ) {
-      // not null && not at image data border , but near othercolor
     } else {
       const r = imageData.data[4 * i + 0];
       const g = imageData.data[4 * i + 1];
@@ -133,5 +219,14 @@ function borderPureColor(imageData, srcHexColor, destHexColor) {
 
   return newImageData;
 }
-var xxxyyy = borderPureColor(imgD, "#FF0000", "#00FF00");
-ctx.putImageData(xxxyyy, 100, 10);
+
+const btn = document.querySelector("#btn");
+btn.addEventListener("click", () => {
+  const colorSource = document.querySelector("#colorSource").value;
+  const colorBorder = document.querySelector("#colorBorder").value;
+  const colorNeighbor = document.querySelector("#colorNeighbor").value;
+  var xxxyyy = borderPureColor(imgD, colorSource, colorBorder, colorNeighbor);
+
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.putImageData(xxxyyy, 10, 10);
+});
